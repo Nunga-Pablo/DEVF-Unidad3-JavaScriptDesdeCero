d605dc8c37051d6484e38deacb71d533ae093599
Pablo Nunga <pablonungaray@protonmail.com>
2025-02-04 20:20:51 -0600

    Proyecto 1: Introducción a JavaScript

---
+++ tipos-de-dato.js
@@ -0,0 +1,55 @@
+// Este es un comentario en JavaScript
+
+// Console.log es una funcion que se utiliza para imprimir en consola
+
+console.log("Hola Mundo");
+
+//Declaracion de variables
+var nombre = "Pablo"; // String
+var edad = 23; // Number
+var sueldo = 1.234 // Number
+var casada = false // Boolean
+var vacunas = true;// Boolean
+var hijos = null; // Null
+var dato; // Undefined
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(vacunas));
+console.log(typeof(hijos));
+console.log(typeof(datos));
+
+//la semantica es importante, significado de las palabras
+
+var nombre = "Francia";
+var pais = 20;
+
+//Si ponemos typeof de null dira que es objeto, pero en realidad es ausencia intencional de valor. 
+// Esto es debido a un error en JavaScript que no se ha corregido por compatibilidad con versiones anteriores 
+
+
+// NaN (Not a Number) es un valor especial que indica que una operacion matematica no es un numero 
+console.log(10 * "Pablo");
+
+/* Comentarios
+ en bloque multilinea */
+
+ //Ejercicios de CAMPUS
+
+console.log(typeof(42));
+console.log(typeof('Veinticinco'));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof(''));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(FALSE));
+console.log(typeof(3.141592));
+console.log(typeof("Pablo"));
+console.log(typeof('+'));
+console.log(typeof(155154575878448488));
+console.log(typeof(x));
+
