dadd6e806363dc93427a0e4d0bbfb0443e8bb000
Pablo Nunga <pablonungaray@protonmail.com>
2025-02-06 20:21:39 -0600

    Proyecto 2: Comparaciones y decisiones en JavaScript

---
+++ proyecto2.js
@@ -0,0 +1,15 @@
+nota = 72;
+
+if(nota >= 90)
+{
+    console.log("Tu nota es: Excelente");
+} else if(nota <= 89 && nota >= 75)
+{
+    console.log("Tu nota es: Bien");
+} else if(nota <= 74 && nota >= 60)
+{
+    console.log("Tu nota es: Suficiente");
+} else 
+{
+    console.log("Tu nota es: No aprobado");
+}
